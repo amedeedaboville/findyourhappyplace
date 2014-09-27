5210467b6584984119d0d2a2b1cbd15eabc05c10
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-09-27 16:07:06 -0400

    update to express app

--- webapp/server.js
+++ webapp/server.js
@@ -1,6 +1,5 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var express = require('express');
+var app = express();
 
 var testvals = [
         [45.507, -73.556, 3],
@@ -12,9 +11,10 @@ var testvals = [
         [45.513, -73.556, 2],
         [45.514, -73.513, 1]
 ];
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(testvals);
-    res.end(index);
-}).listen(9615);
+app.get('/data', function(req, res){
+      res.send(testvals);
+});
 
+var server = app.listen(3000, function() {
+        console.log('Listening on port %d', server.address().port);
+});
