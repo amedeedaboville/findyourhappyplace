eee134fa00aad889d628c124520437e542851e41
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-09-27 15:51:59 -0400

    changed test data format

--- webapp/server.js
+++ webapp/server.js
@@ -2,19 +2,19 @@ var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync('index.html');
 
+var testvals = [
+        [45.507, -73.556, 3],
+        [45.508, -73.556, 3],
+        [45.509, -73.556, 2],
+        [45.510, -73.556, 1],
+        [45.511, -73.556, 2],
+        [45.512, -73.556, 4],
+        [45.513, -73.556, 2],
+        [45.514, -73.513, 1]
+];
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write(testvals);
     res.end(index);
 }).listen(9615);
 
-var testvals = [
-{(45.507, -73.556), weight: 3}
-{(45.508, -73.556), weight: 3}
-{(45.509, -73.556), weight: 2}
-{(45.510, -73.556), weight: 1}
-{(45.511, -73.556), weight: 2}
-{(45.512, -73.556), weight: 4}
-{(45.513, -73.556), weight: 2}
-{(45.514, -73.513), weight: 1}
-];
