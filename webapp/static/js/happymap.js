feb5c3ba97a1b5c2986eec3e9967ab8b91c875a2
root <droplet@happyplace.com>
2014-09-28 11:58:33 -0400

    Sam's changes.

--- webapp/static/js/happymap.js
+++ webapp/static/js/happymap.js
@@ -160,6 +160,63 @@ $.ajax({
     }
 });
 
+var input = (document.getElementById('pac-input'));
+
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+var searchBox = new google.maps.places.SearchBox((input));
+
+// Listen for the event fired when the user selects an item from the
+// pick list. Retrieve the matching places for that item.
+google.maps.event.addListener(searchBox, 'places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+                return;
+        }
+        for (var i = 0, marker; marker = markers[i]; i++) {
+                marker.setMap(null);
+        }
+
+        // For each place, get the icon, place name, and location.
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        console.log("PLACES: "+places[0]);
+        //map.setCenter = new google.maps.LatLng(places[0] );
+
+        for (var i = 0, place; place = places[i]; i++) {
+                var image = {
+                        url: place.icon,
+                        size: new google.maps.Size(71, 71),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(17, 34),
+                        scaledSize: new google.maps.Size(25, 25)
+                };
+
+                // Create a marker for each place.
+                var marker = new google.maps.Marker({
+                        map: map,
+                        icon: image,
+                        title: place.name,
+                        position: place.geometry.location
+                });
+
+                markers.push(marker);
+
+                bounds.extend(place.geometry.location);
+        }
+
+        map.fitBounds(bounds);
+});
+
+// Bias the SearchBox results towards places that are within the bounds of the
+// current map's viewport.
+google.maps.event.addListener(map, 'bounds_changed', function() {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
+});
+
+
 /*
 function makeMarker(x){
 	var coord = new google.maps.LatLng(x[0],x[1]);
