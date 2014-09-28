47b6de982c94a34d87ef1abcb15092b446a82011
Tara Campbell <tara0campbell@gmail.com>
2014-09-28 11:45:28 -0400

    Add gradient, update radius.

--- webapp/static/js/happymap.js
+++ webapp/static/js/happymap.js
@@ -128,6 +128,31 @@ $.ajax({
         });
 
         heatmap.setMap(map);
+
+        function changeGradient() {
+          var gradient = [
+            'rgba(0, 255, 255, 0)',
+            'rgba(0, 255, 255, 1)',
+            'rgba(0, 191, 255, 1)',
+            'rgba(0, 127, 255, 1)',
+            'rgba(0, 63, 255, 1)',
+            'rgba(0, 0, 255, 1)',
+            'rgba(0, 0, 223, 1)',
+            'rgba(0, 0, 191, 1)',
+            'rgba(0, 0, 159, 1)',
+            'rgba(0, 0, 127, 1)',
+            'rgba(63, 0, 91, 1)',
+            'rgba(127, 0, 63, 1)',
+            'rgba(191, 0, 31, 1)',
+            'rgba(255, 0, 0, 1)'
+          ]
+          heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+        }
+
+        function changeRadius() {
+          heatmap.set('radius', heatmap.get('radius') ? null : 20);
+        }
+
     },
     error: function(xhr, desc, err) {
         console.log(xhr);
